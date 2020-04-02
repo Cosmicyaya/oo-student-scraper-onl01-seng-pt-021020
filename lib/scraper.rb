59dcadbf747e76e7c0c76a5d0c4a21af33e50963
Ryan Hollins <ballbaroi@gmail.com>
2020-04-02 19:05:10 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,41 @@
-require 'pry'
 require 'open-uri'
-require 'nokogiri'
+require 'pry'
 
 class Scraper
+
   def self.scrape_index_page(index_url)
-    students = []
     html = open(index_url)
-    index = Nokogiri::HTML(html)
-    index.css("div.roster-cards-container").each do |card|
-      card.css(".student-card").each do |student|
-        scraped_students = {}
-        scraped_students[:name] = student.css("h4.student-name").text
-        scraped_students[:location] = student.css("p.student-location").text
-        profile_path = student.css("a").attribute("href").value
-        #scraped_students[:profile_url] = '"h3.view-profile-div'.value
-        students << scraped_students
-      end
+    doc = Nokogiri::HTML(html)
+    student_cards = doc.css(".student-card a")
+    student_cards.collect do |element|
+      {:name => element.css(".student-name").text ,
+        :location => element.css(".student-location").text,
+        :profile_url => element.attr('href')
+      }
     end
-    students
   end
+
+  def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    return_hash = {}
+
+      social = doc.css(".vitals-container .social-icon-container a")
+      social.each do |element| #iterate through each of the social elements and assign the keys if the item exists
+        if element.attr('href').include?("twitter")
+          return_hash[:twitter] = element.attr('href')
+        elsif element.attr('href').include?("linkedin")
+          return_hash[:linkedin] = element.attr('href')
+        elsif element.attr('href').include?("github")
+          return_hash[:github] = element.attr('href')
+        elsif element.attr('href').end_with?("com/")
+          return_hash[:blog] = element.attr('href')
+        end
+      end
+      return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+      return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
+
+  return_hash
+  end
+
 end
